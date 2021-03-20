4cce44e721bab0786b5325d6aca2de506ecbbd74
Kevin Petit <petitkevin20@gmail.com>
2021-03-19 21:31:05 -0400

    views and routes changes

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
